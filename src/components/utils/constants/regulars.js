8b97859bc049392e21630e10f9c8fcc9b49f3890
Juliet-cyborg <juliiaj98@gmail.com>
2025-03-14 17:47:11 +0200

    create input for formulas

---
+++ src/components/utils/constants/regulars.js
@@ -0,0 +1,7 @@
+export const mathOperands = /^[-+*/]?\d+$/
+
+export const operatorPrefixPattern  = /^([+\-*/])(.+)$/
+
+export const operatorStartRegex  = /^[+\-*/]/
+
+export const numberBeforeOperatorPattern = /^\d+[+\-*/].+/;
