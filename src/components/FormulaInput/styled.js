e6cec96093899f36e75c9086033a62dec4045013
July <85956057+Juliet-cyborg@users.noreply.github.com>
2025-03-14 18:33:49 +0200

    check deploy (#2)

--- src/components/FormulaInput/styled.js
+++ src/components/FormulaInput/styled.js
@@ -5,6 +5,7 @@ export const FormulaContainerStyled = styled(Box)`
     border: 1px solid #ccc;
     padding: 8px;
     border-radius: 4px;
-    min-height: 40px
+    min-height: 40px;
+    background: gray;
 `;
 
